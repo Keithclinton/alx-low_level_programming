1f709bf8dfa785af80988def2abcd677c2fb157c
Keithclinton <keithnyaburi@gmail.com>
2023-10-06 02:52:49 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this is the function of task 2
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+for (i = 'a', i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
